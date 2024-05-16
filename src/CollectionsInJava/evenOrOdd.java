476ab4664bb33c1f082aa7478e9b9a497ae0e76e
spogulak <srikanthpogulakonda@gmail.com>
2024-05-16 12:51:14 +0530

    firstcommit

---
+++ src/CollectionsInJava/evenOrOdd.java
@@ -0,0 +1,18 @@
+package CollectionsInJava;
+
+import java.util.Scanner;
+
+public class evenOrOdd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("enter number to check even or odd");
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		if(n%2==0) {
+		System.out.println("The given number is even:");
+		}else
+			System.out.println("The given number is odd:");
+		}
+
+}
